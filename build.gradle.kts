32541ce5a1443ca5eacd52c6c0d1213fda4c0593
Tseng <152485397+Hc0712@user.noreply.github.com>
2026-04-24 12:16:05 +0800

    1) align all version in gradle wrapper to ver. 8.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@
  * - bridge_v3.1 only adds debug logging and code comments; no feature logic is intentionally changed.
  */
 plugins {
-    id("com.android.application") version "8.13.0" apply false
+    id("com.android.application") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
 }
